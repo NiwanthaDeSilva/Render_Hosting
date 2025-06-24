f65b27130da71df08067881c78c98882f668fbf2
NiwanthaDeSilva <niwantha444@gmail.com>
2025-06-24 12:28:16 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the client directory
+app.use(express.static(path.join(__dirname, '../client')));
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
+});
+
+// Fallback to index.html for SPA routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/render front.html'));
 });
 
 app.listen(PORT, () => {
