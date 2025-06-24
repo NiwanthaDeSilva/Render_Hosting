bb6bd91dc6a8da08f338988e6283dfd2ad2915e1
NiwanthaDeSilva <niwantha444@gmail.com>
2025-06-24 13:04:46 +0530

    editd

--- server/index.js
+++ server/index.js
@@ -1,20 +1,9 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Serve static files from the client directory
-app.use(express.static(path.join(__dirname, '../client')));
-
+// API route
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
-// Fallback to index.html for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/render front.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
